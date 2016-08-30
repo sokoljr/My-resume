6b55ee9c01cd42cf03204710894ff5e3df93e4ac
sokoljr <Serg607.sokol@gmail.com>
2016-08-30 23:33:08 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	Cufon.replace('h1', {
+		fontSize: '60px', 
+		fontWeight: 'bold', 
+		color: '#333'
+	});
+	Cufon.replace('h2', {
+		fontSize: '40px', 
+		fontWeight: 'bold',
+		fontStyle: 'italic',
+		color: '#173D5F'
+	});
+	Cufon.replace('h3', {
+		fontSize: '35px', 
+		fontWeight: 'bold',
+		color: '#333'
+	});
+	Cufon.replace('h4', {
+		fontSize: '28px', 
+		fontWeight: 'bold',
+		fontStyle: 'italic',
+		color: '#173D5F'
+	});
+	Cufon.replace('h5', {
+		fontSize: '18px', 
+		fontWeight: 'normal',
+		color: 'white'
+	});
+	Cufon.replace('.about p', {
+		fontSize: '22px', 
+		fontWeight: 'normal',
+		color: '#333'
+	});
+	Cufon.replace('.contact>ul>li>div>p, .contact>ul>li>div>a', {
+		fontSize: '22px'
+	});
+});
