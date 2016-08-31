f9f691ce4d701fd4a41215c5d5daef12031d4d29
sokoljr <Serg607.sokol@gmail.com>
2016-09-01 01:47:49 +0300

    form input

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
 		fontWeight: 'normal',
 		color: 'white'
 	});
-	Cufon.replace('.about p', {
+	Cufon.replace('.about p, .form-group label', {
 		fontSize: '22px', 
 		fontWeight: 'normal',
 		color: '#333'
@@ -34,16 +34,81 @@ $(document).ready(function(){
 	Cufon.replace('.contact>ul>li>div>p, .contact>ul>li>div>a', {
 		fontSize: '22px'
 	});
+	Cufon.replace('.form-group button, .modal-footer button', {
+		fontSize: '18px',
+		letterSpacing: '1px', 
+		fontWeight: 'normal',
+		color: 'white'
+	});
 
 	$('.print').click(function() {
      	window.print();
 	});
 
-	$('#exampleModal').on('show.bs.modal', function (event) {
-		var button = $(event.relatedTarget) 
-		var recipient = button.data('whatever') 
-		var modal = $(this)
-		modal.find('.modal-title').text('Online contact')
-		modal.find('.modal-body input').val(recipient)
+	$('#exampleModal').on('show.bs.modal', function(event){});
+	$('#fotoModal').on('show.bs.modal', function(event){});
+
+	// Form validation
+	
+	var form = $('#main_form');
+	
+	form.on("submit", function(e){
+		e.preventDefault();
+		
+		var name = form.find("#name").val();
+		var nameLen = form.find("#name").val().length;
+		var email = form.find("#email").val();
+		var number = form.find("#phone_number").val();
+		var numberLen = number.length;
+		var errors = $("p.errors");
+		
+		$(".delegation").on("input", "input", function(e) {
+		$(this).removeClass("border_red");
+		errors.html("");
+		});
+		
+		if (name === ""){
+			errors.html("Please enter Your name!");
+			$("#name").addClass("border_red");
+			return;
+		}
+		
+		if (nameLen < 2) {
+		errors.html("Your name is too short!");
+		$("#name").addClass("border_red");
+		return;
+		}
+		
+		if (email === ""){
+			errors.html("You have not entered email!");
+			$("#email").addClass("border_red");
+			return;
+		}
+		var emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		var validEmail = emailRegexp.test(email);
+		if (!validEmail) {
+			errors.html("Invalid email!");
+			$("#email").addClass("border_red");
+			return;
+		}
+		
+		if (number === ""){
+			errors.html("You have not entered phone number!");
+			$("#phone_number").addClass("border_red");
+			return;
+		}
+		
+		if (isNaN(parseInt(number))){
+			errors.html("Number must consist only digits!");
+			$("#phone_number").addClass("border_red");
+			return;
+		}
+		
+		if (numberLen < 6) {
+		errors.html("Entered phone number is too short!");
+		$("#phone_number").addClass("border_red");
+		return;
+		}
+		this.submit();
 	});
 });
